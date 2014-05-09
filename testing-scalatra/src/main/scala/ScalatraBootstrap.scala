7c4eb948acab72b8c8734466727046648ba85160
Orlando Karam <orlando.karam@gmail.com>
2014-05-08 17:08:35 -0700

    first scalatra proj

---
+++ testing-scalatra/src/main/scala/ScalatraBootstrap.scala
@@ -0,0 +1,9 @@
+import com.github.okaram.scalatra1._
+import org.scalatra._
+import javax.servlet.ServletContext
+
+class ScalatraBootstrap extends LifeCycle {
+  override def init(context: ServletContext) {
+    context.mount(new MyScalatraServlet, "/*")
+  }
+}
---
+++ testing-scalatra/src/main/scala/com/github/okaram/scalatra1/MyScalatraServlet.scala
@@ -0,0 +1,17 @@
+package com.github.okaram.scalatra1
+
+import org.scalatra._
+import scalate.ScalateSupport
+
+class MyScalatraServlet extends TestingScalatraStack {
+
+  get("/") {
+    <html>
+      <body>
+        <h1>Hello, world!</h1>
+        Say <a href="hello-scalate">hello to Scalate</a>.
+      </body>
+    </html>
+  }
+  
+}
