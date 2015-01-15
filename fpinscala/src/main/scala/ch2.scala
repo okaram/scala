ed208538703e91df590c30fbb3e0bb574bb6a385
Orlando Karam <orlando.karam@gmail.com>
2015-01-15 08:47:22 -0800

    ch2, curry, uncurry, compose

--- fpinscala/src/main/scala/ch2.scala
+++ fpinscala/src/main/scala/ch2.scala
@@ -26,4 +26,19 @@ object ch2 {
 		}
 		return true;
 	}
+
+	// this functions transforms a function of 2 args into a function of one arg, that returns a function taking the second arg
+	def curry[A,B,C](f:(A,B)=>C):A=>(B=>C) = {
+		(a:A) => ((b:B) => f(a,b)); 
+	}
+
+	// this reverses the currying
+	def uncurry[A,B,C](f:A=>(B=>C)):(A,B)=>C = {
+		(a:A,b:B) => f(a)(b)
+	}
+
+	def compose[A,B,C](f:B=>C, g:A=>B): A=>C = {
+		(a:A)=>f(g(a))
+	}
+
 }
